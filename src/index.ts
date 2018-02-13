77e755d384678914f7549db31913177a9327dd2d
Jeff Stice-Hall <jsh@digitalmaelstrom.net>
2018-02-12 18:47:27 -0600

    Inital commit

---
+++ src/index.ts
@@ -0,0 +1,14 @@
+class Child {
+    name: string;
+    age: number;
+}
+
+let child = new Child();
+child.name = "Ethan";
+child.age = 14;
+
+let child2 = new Child();
+child2.name = "Amber";
+child2.age = 19;
+
+console.log(child, child2);
